a2a635c4662c6f35c60c0fd592f8d3b4d73f6acf
Gayathri Sivakumar Menon <60652234+gayathrismenon@users.noreply.github.com>
2021-08-18 20:01:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let a=0
+function add()
+{
+    a++
+   
+    document.getElementById('screen').innerHTML=a
+      
+}
+function sub()
+{
+    a--
+    
+            document.getElementById('screen').innerHTML=a
+       
+}
+function reset()
+{
+    a=0
+    
+            document.getElementById('screen').innerHTML=a
+    
+}
